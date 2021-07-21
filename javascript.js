dd5558d1c1c0839e09a545f7e41f6915e23ec750
IUC-V <87781501+IUC-V@users.noreply.github.com>
2021-07-22 01:08:00 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+    alert("Thank you, find the list of centers nearby!");
+ 
+  document.getElementById('testForm').addEventListener('submit',submitForm);
+  
+  /* function store input values in variables */
+  function submitForm(e){
+    e.preventDefault();
+    var fname =getInputVal('firstname');
+    alert("Thank you, find the list of centers nearby!");
+}
+    
+
+
+
+
+
